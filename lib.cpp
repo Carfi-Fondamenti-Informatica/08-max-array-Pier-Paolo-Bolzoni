a5e20887134426b9f7b73c3cc363f28e60b485fb
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2023-05-23 01:46:02 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
+double valoremax(double lista[], int dim){
+    double max = lista[0];
+    for(int i=1;i<dim;i++){
+        if(lista[i]>max){
+            max = lista[i];
 
+        }
+    }
+    return max;
+}
