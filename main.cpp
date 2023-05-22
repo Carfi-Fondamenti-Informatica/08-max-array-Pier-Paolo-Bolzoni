a79d4da39a4e8fc57b29b93731e888915481ed31
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2023-05-23 01:45:47 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+
+    int dim;
+    cin >> dim ;
+    double lista[dim];
+
+    for(int i=0;i<dim;i++){
+        cin >> lista[i];
+    }
+    double numero = valoremax(lista, dim);
+    cout << numero << endl;
+    return 0;
 }
