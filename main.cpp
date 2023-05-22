19adba60d52f4ce084f3ec875eb39d65791ac0e2
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2023-05-23 01:53:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main() {
     for(int i=0;i<dim;i++){
         cin >> lista[i];
     }
-    num = valoremax(lista, dim);
+    double num = valoremax(lista, dim);
     cout << num << endl;
     return 0;
 }
